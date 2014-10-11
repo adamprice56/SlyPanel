29c6e3cefeac4094890577afcbbbedf5f16c909c
adamprice56 <adam.price56@gmail.com>
2014-10-12 00:01:11 +0100

    Initial Commit
    -SSH is not working-
    
    -Timer crashes app on launch-
    
    -Refresh button & fragment switching is working fine-

---
+++ app/src/main/java/adam/price/slypanel/CreateConnection.java
@@ -0,0 +1,26 @@
+package adam.price.slypanel;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by root on 02/10/14.
+ */
+public class CreateConnection extends Fragment {
+
+    public CreateConnection() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View connectionView = inflater.inflate(R.layout.fragment_create_connection, container, false);
+
+        return (connectionView);
+    }
+}
